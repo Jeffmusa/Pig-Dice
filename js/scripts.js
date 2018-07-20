bb6d0ed5bf0eb6cb58a844b20bb7fbbe1bde5b65
Jeff Musa <jeffmusa@gmail.com>
2018-07-20 12:52:19 +0300

    modified js

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ function Player (name,turnTotal,diceRoll,overallScore,active){
   this.overallScore=0;
   this.active=active;
 }
+
+// enabling and disabling gaming areas according to active player
+function activeUser() {
+  if (player1.active === true && player2.active === false) {
+    $('.player1Area').children().prop('disabled',false);
+    $('.player1Area').removeClass('disableGamingArea');
+    $('.player2Area').children().prop()('disabled',true);
+    $('.player2Area').addClass('disableGamingArea');
+  }else {
+    $('.player1Area').children().prop('disabled',true);
+
+  }
+}
